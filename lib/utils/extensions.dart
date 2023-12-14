e5a39bb83fa8fd36b949c0f955a29c3d0d4f6956
Fernando Mansilla <fernando.mansilla@etermax.com>
2023-12-13 21:25:25 -0300

    chore: Add Object and String extensions

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -2,4 +2,25 @@
 extension ObjectExtensions<T extends Object> on T {
   T? takeIf(bool condition) => condition ? this : null;
   T? takeIfDef(Object? object) => object != null? this : null;
+  R? map<R>(R Function(T) map) => map(this);
+}
+
+extension StringExtensions on String {
+  String trimIndent() {
+    final List<String> lines = split('\n');
+
+    // Find the minimum indentation level
+    int minIndentation = lines
+        .where((line) => line.trim().isNotEmpty)
+        .map((line) => line.indexOf(RegExp(r'\S')))
+        .reduce((minIndent, currentIndent) => (currentIndent < minIndent) ? currentIndent : minIndent);
+
+    // Remove the common indentation from each line
+    final trimmedLines = lines.map((line) => line.substring(minIndentation));
+
+    // Join the lines back together
+    final result = trimmedLines.join('\n');
+
+    return result;
+  }
 }
