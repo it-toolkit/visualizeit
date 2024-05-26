37931f6794a1e6f24c91fe07c2089e553e59b899
Osiris <osirisarguello@gmail.com>
2024-05-26 18:16:19 -0300

    Modificado nombre del ejemplo complejo y agregado comando de busqueda en ejemplo de extension

--- lib/wiring/repositories.dart
+++ lib/wiring/repositories.dart
@@ -27,7 +27,7 @@ extension GetItRepositories on GetIt {
       "assets/script_examples/global_commands_example.yaml",
       "assets/script_examples/extension_slides_example.yaml",
       "assets/script_examples/extension_bsharptree_example.yaml",
-      "assets/script_examples/bsharptre_example_with_explanation.yaml"
+      "assets/script_examples/bsharptree_example_with_explanation.yaml"
     ];
     return Future.wait(assetKeys.map((key) async =>
         RawScript(key.hashCode.toString(), await rootBundle.loadString(key))));
