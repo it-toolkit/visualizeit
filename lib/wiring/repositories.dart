b96263b21f147de13f82b1beb9d9a12fbf3d899d
Fernando Mansilla <fernando.e.mansilla@gmail.com>
2024-07-04 12:57:16 -0300

    fix: Show example public scripts in release mode

--- lib/wiring/repositories.dart
+++ lib/wiring/repositories.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:visualizeit/extension/domain/extension_repository.dart';
@@ -9,13 +8,10 @@ import '../scripting/infrastructure/script_repository.dart';
 
 extension GetItRepositories on GetIt {
   void registerRepositories() {
-    registerLazySingleton<ScriptRepository>(() => kReleaseMode
-        ? InMemoryScriptRepository(get())
-        : InMemoryScriptRepository(
-            get(),
-            initialRawScriptsLoader: _loadExampleScriptsFromAssets()),
-            instanceName: "publicScriptsRepository"
-        );
+    registerLazySingleton<ScriptRepository>(() =>
+        InMemoryScriptRepository(get(), initialRawScriptsLoader: _loadScriptsFromAssets(_publicScriptKeys)),
+        instanceName: "publicScriptsRepository"
+    );
 
     registerLazySingleton<ScriptRepository>(() =>
         InMemoryScriptRepository(get()),
@@ -30,24 +26,24 @@ extension GetItRepositories on GetIt {
         () async => await DefaultExtensionRepository.withAvailableExtensions());
   }
 
-  Future<List<RawScript>> _loadExampleScriptsFromAssets() async {
-    //TODO also read from extensions
-    final assetKeys = [
-      "assets/script_examples/visualizeit_intro.yaml",
-      "assets/script_examples/bsharptree_example_with_explanation.yaml",
-      "assets/script_examples/extension_externalsort_example.yaml",
-      "assets/script_examples/global_commands_example.yaml",
-      "assets/script_examples/multi_scene_example.yaml",
-      "assets/script_examples/extension_slides_example.yaml",
-      "assets/script_examples/extension_bsharptree_example.yaml",
-      "assets/script_examples/extension_bsharptree_incremental_example.yaml",
-      "assets/script_examples/extension_template_example.yaml",
-      "assets/script_examples/extension_extendiblehashing_bucket_overflow_example.yaml",
-      "assets/script_examples/extension_extendiblehashing_hashtable_update_example.yaml",
-      "assets/script_examples/extension_extendiblehashing_bucket_freed_example.yaml",
-      "assets/script_examples/extension_extendiblehashing_bucket_empty_example.yaml",
-    ];
+  Future<List<RawScript>> _loadScriptsFromAssets(List<String> assetKeys) async {
     return Future.wait(assetKeys.map((key) async =>
         RawScript(key.hashCode.toString(), await rootBundle.loadString(key))));
   }
+
+  static final _publicScriptKeys = [
+    "assets/script_examples/visualizeit_intro.yaml",
+    "assets/script_examples/bsharptree_example_with_explanation.yaml",
+    "assets/script_examples/extension_externalsort_example.yaml",
+    "assets/script_examples/global_commands_example.yaml",
+    "assets/script_examples/multi_scene_example.yaml",
+    "assets/script_examples/extension_slides_example.yaml",
+    "assets/script_examples/extension_bsharptree_example.yaml",
+    "assets/script_examples/extension_bsharptree_incremental_example.yaml",
+    "assets/script_examples/extension_template_example.yaml",
+    "assets/script_examples/extension_extendiblehashing_bucket_overflow_example.yaml",
+    "assets/script_examples/extension_extendiblehashing_hashtable_update_example.yaml",
+    "assets/script_examples/extension_extendiblehashing_bucket_freed_example.yaml",
+    "assets/script_examples/extension_extendiblehashing_bucket_empty_example.yaml",
+  ];
 }
